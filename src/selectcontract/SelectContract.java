5fa8b26850924340d86d7417e9d273795ace9fd3
Ryan Dickson <rdickson.developer@gmail.com>
2022-06-29 11:27:43 -0700

    First Commit - End of Lab Step 4 (part A)

---
+++ src/selectcontract/SelectContract.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package selectcontract;
+
+/**
+ *
+ * @author ryandickson
+ */
+public class SelectContract {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        ContractView theView = new ContractView();
+        ContractModel theModel = new ContractModel();
+        ContractController theController;
+        theController = new ContractController(theView, theModel);
+        theView.setVisible(true);
+    }
+    
+}
